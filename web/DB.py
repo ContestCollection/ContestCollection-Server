40c53530c67e4ca3d054a2f9b9986a496741cf84
SonChangHa <jjang00198@gmail.com>
2022-04-10 15:24:00 +0900

    DB complete

---
+++ web/DB.py
@@ -0,0 +1,48 @@
+import sqlite3
+import json
+
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+def mkDB():
+    #id 추가해야함
+    cur.execute("""CREATE TABLE contestData(
+    part TEXT, 
+    year INTEGER, 
+    teamNum INTEGER,
+    name TEXT, 
+    award TEXT, 
+    subTitle TEXT, 
+    summary TEXT,
+    img TEXT, 
+    member1 TEXT, 
+    member2 TEXT, 
+    member3 TEXT, 
+    member4 TEXT, 
+    calendar TEXT, 
+    githubLink TEXT,
+    youtubeLink TEXT, 
+    serviceLink TEXT, 
+    skills TEXT)
+    """)
+    # 16개
+
+
+def insertToDB():
+    with open("data.txt", "r", encoding='UTF-8') as f:
+        temp = []
+        for t in f.readlines():
+            temp.append(t.strip())
+        cur.execute("INSERT INTO contestData VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", temp)
+        conn.commit()
+        conn.close()
+        print("data insert complete")
+
+def getFromDB():
+    cur.execute("SELECT rowid, * FROM contestData")
+    rows = cur.fetchall()
+    conn.close()
+
+    print(rows)
+
+getFromDB()
