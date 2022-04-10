419f7a615d322bd6e72442bc3211464155ea7c22
SonChangHa <jjang00198@gmail.com>
2022-04-10 16:41:02 +0900

    restapi done

--- web/readJson.py
+++ web/readJson.py
@@ -2,7 +2,8 @@ import json
 
 
 def read_json():
-    with open('test1.json', 'r', encoding='UTF-8') as f:
+    with open('test.json', 'r', encoding='UTF-8') as f:
         json_data = json.load(f)
-
-        return json.dumps(json_data)
+        #굳이 덤프 안해도 됨. 하면 dict -> str로 바뀌는건데 안하는게 보기 편함
+        #return json.dumps(json_data)
+        return json_data
