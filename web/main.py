4e029463d4e8e18988bd685bafeba201abb9a365
SonChangHa <jjang00198@gmail.com>
2022-06-16 12:07:42 +0900

    DB.py 오류 수정

--- web/main.py
+++ web/main.py
@@ -1,17 +1,19 @@
 import os
 import werkzeug
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, request, current_app
 from flask_restful import Resource, Api, reqparse
 import readJson
 import makeJson
 from web import makeJsonForios
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import DB
 import openpyxl as oxl
+from functools import update_wrapper
+from datetime import timedelta
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-CORS(app)
+CORS(app, resources={r'*': {'origins': '*'}})
 api = Api(app)
 
 
@@ -62,9 +64,51 @@ class Upload(Resource):
 
                 text = column[1:19]
                 DB.insertToDB(text)
-                
+
 
             return jsonify({'result':file_path})
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 api.add_resource(Upload, '/upload')
 
